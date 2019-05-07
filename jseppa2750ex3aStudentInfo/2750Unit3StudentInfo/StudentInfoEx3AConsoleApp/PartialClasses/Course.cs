5fefc393fc54a8d1c94d409a54d269c266991746
Joshua Seppa <GX8386DK@go.minnstate.edu>
2019-05-07 17:38:55 -0500

    Ex3A complications

---
+++ jseppa2750ex3aStudentInfo/2750Unit3StudentInfo/StudentInfoEx3AConsoleApp/PartialClasses/Course.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoEx3AConsoleApp
+{
+    public partial class Course
+    {
+        public Course(int id, String courseNum, String courseTitle, short credits, Department department)
+        {
+            this.Sections = new HashSet<Section>();
+            this.Id = id;
+            this.CourseNum = courseNum;
+            this.CourseTitle = courseTitle;
+            this.Credits = credits;
+            this.Department = department;
+            department.Courses.Add(this);
+        }
+
+        public Section FindSection(int id)
+        {
+            // based on sectionId
+            Section foundSection = null;
+            foreach (Section c in this.Sections)
+            {
+                if (c.Id == id)
+                {
+                    foundSection = c;
+                    break;
+                }
+            }
+            return foundSection;
+        }
+    }
+}
---
+++ jseppa2750ex3aStudentInfo/2750Unit3StudentInfo/StudentInfoEx3AConsoleApp/PartialClasses/Section.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoEx3AConsoleApp
+{
+    public partial class Section
+    {
+        public Section(int id, int capacity, Course course, Term term, Assignment assignment, Enrollment enrollment)
+        {
+            this.Enrollments = new HashSet<Enrollment>();
+            this.Assignments = new HashSet<Assignment>();
+            this.Id = id;
+            this.Capacity = capacity;
+            this.Course = course;
+            this.Term = term;
+            this.Assignments = Assignments;
+            this.Enrollments = Enrollments;
+            course.Sections.Add(this);
+        }
+        public Section(int id, int capacity, Course course, Term term)
+        {
+            this.Id = id;
+            this.Capacity = capacity;
+            this.Course = course;
+            this.Term = term;
+            course.Sections.Add(this);
+        }
+    }
+}
